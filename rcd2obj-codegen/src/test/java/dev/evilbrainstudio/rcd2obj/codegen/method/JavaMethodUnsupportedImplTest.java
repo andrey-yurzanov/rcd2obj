f12595e7208f4b8b30cb4d6c2ce4b3194139fcda
andrey-yurzanov <yurzanov.andrey@gmail.com>
2023-07-20 22:28:01 +0300

    Method implementation Issue: #5

---
+++ rcd2obj-codegen/src/test/java/dev/evilbrainstudio/rcd2obj/codegen/method/JavaMethodUnsupportedImplTest.java
@@ -0,0 +1,41 @@
+/*
+ *    Copyright 2023 Evil Brain Studio
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package dev.evilbrainstudio.rcd2obj.codegen.method;
+
+import dev.evilbrainstudio.rcd2obj.codegen.render.JavaElementWriteRender;
+import java.io.StringWriter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests of the unsupported method's implementation.
+ *
+ * @author Andrey_Yurzanov
+ */
+class JavaMethodUnsupportedImplTest {
+  private static final String EXPECTED_VALUE = "{thrownewjava.lang.UnsupportedOperationException();}";
+
+  @Test
+  void renderTest() {
+    StringWriter writer = new StringWriter();
+
+    JavaMethodUnsupportedImpl impl = new JavaMethodUnsupportedImpl();
+    impl.render(new JavaElementWriteRender(writer));
+
+    Assertions.assertEquals(EXPECTED_VALUE, writer.toString());
+  }
+}
