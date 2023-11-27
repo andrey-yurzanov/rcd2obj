57bdfcc3eff48d04caff37421d73e82bf2e94675
andrey-yurzanov <yurzanov.andey@gmail.com>
2023-11-28 00:43:36 +0300

    Simple codegen plugin Issue: #18

---
+++ rcd2obj-codegen-plugin/src/main/java/dev/evilbrainstudio/rcd2obj/codegen/plugin/JavaSourceCode.java
@@ -0,0 +1,74 @@
+/*
+ *    Copyright 2023 Evil Brain Studio
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package dev.evilbrainstudio.rcd2obj.codegen.plugin;
+
+import dev.evilbrainstudio.rcd2obj.annotation.mapping.Mapper;
+import dev.evilbrainstudio.rcd2obj.annotation.meta.TableMetaInfo;
+import dev.evilbrainstudio.rcd2obj.codegen.JavaClass;
+import dev.evilbrainstudio.rcd2obj.codegen.JavaClassPackage;
+import dev.evilbrainstudio.rcd2obj.codegen.inherited.JavaInheritableElement;
+import dev.evilbrainstudio.rcd2obj.codegen.render.JavaElementWriteRender;
+import dev.evilbrainstudio.rcd2obj.codegen.render.format.JavaElementFormatRender;
+
+import java.io.StringWriter;
+
+/**
+ * The generated Java's source code.
+ *
+ * @author Andrey_Yurzanov
+ */
+public class JavaSourceCode {
+  private final StringWriter writer;
+  private final String name;
+  private final JavaClass javaClass;
+
+  /**
+   * Constructs new instance of Java's source code.
+   *
+   * @param info information about annotated entity
+   */
+  public JavaSourceCode(TableMetaInfo info) {
+    Class<?> type = info.getType();
+    this.name = type.getSimpleName().concat("StubMapper");
+    this.writer = new StringWriter();
+    this.javaClass = new JavaClass(name)
+      .setClassPackage(new JavaClassPackage(type.getPackage().getName()))
+      .setClassImplements(new JavaInheritableElement(Mapper.class));
+  }
+
+  /**
+   * Returns generated source code.
+   *
+   * @return generated source code
+   */
+  public String getCode() {
+    String code = writer.toString();
+    if (code.isEmpty()) {
+      javaClass.render(new JavaElementFormatRender(new JavaElementWriteRender(writer)));
+    }
+    return writer.toString();
+  }
+
+  /**
+   * Returns name of the generated class.
+   *
+   * @return name of the generated class
+   */
+  public String getName() {
+    return name;
+  }
+}
---
+++ rcd2obj-codegen-plugin/src/main/java/dev/evilbrainstudio/rcd2obj/codegen/plugin/SourceCodeWriter.java
@@ -0,0 +1,72 @@
+/*
+ *    Copyright 2023 Evil Brain Studio
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package dev.evilbrainstudio.rcd2obj.codegen.plugin;
+
+import dev.evilbrainstudio.rcd2obj.annotation.meta.TableMetaInfo;
+
+import javax.inject.Named;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Writer of generated source code.
+ *
+ * @author Andrey_Yurzanov
+ */
+@Named
+public class SourceCodeWriter {
+  private static final String PACKAGE_SEPARATOR = ".";
+  private static final String FILE_EXTENSION = ".java";
+  private static final String PATH_SEPARATOR = getPathSeparator();
+
+  /**
+   * Writes generated source code.
+   *
+   * @param root       path without name of the file and package
+   * @param info       annotated entity's meta information
+   * @param sourceCode generated source code
+   * @throws IOException when can't write to file
+   */
+  public void write(Path root, TableMetaInfo info, JavaSourceCode sourceCode) throws IOException {
+    Class<?> type = info.getType();
+    Package typePackage = type.getPackage();
+    String packageName = typePackage.getName();
+
+    Path directory = Paths.get(root.toString(), packageName.replace(PACKAGE_SEPARATOR, PATH_SEPARATOR));
+    if (!Files.exists(directory)) {
+      Files.createDirectories(directory);
+    }
+
+    Path filePath = Paths.get(directory.toString(), sourceCode.getName() + FILE_EXTENSION);
+    try (Writer out = new FileWriter(filePath.toFile())) {
+      out.write(sourceCode.getCode());
+      out.flush();
+    }
+  }
+
+  // Returns path's separator of current os
+  private static String getPathSeparator() {
+    FileSystem fileSystem = FileSystems.getDefault();
+    return fileSystem.getSeparator();
+  }
+}
